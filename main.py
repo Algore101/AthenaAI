d37c59efd2aee28e48a693cf016163eb5315ae56
Jaydon Walters <jaydonwalters21@gmail.com>
2023-05-22 17:16:09 +0200

    Replace bot token with environment variable

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ if __name__ == '__main__':
         for line in file:
             line = line.strip()
             settings[line.split('=')[0]] = line.split('=')[1]
-    bot.run_discord_bot(token=settings['token'], prefix=settings['prefix'])
+    bot.run_discord_bot(token=os.getenv('BOT_TOKEN'), prefix=settings['prefix'])
