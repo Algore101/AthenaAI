9f98642522a6ae5f0bf3146e8a0d72c8de4e5a4d
Jaydon Walters <jaydonwalters21@gmail.com>
2023-05-23 15:45:28 +0200

    Changed text commands to slash commands

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 from libraries import bot
 import os
 
-SETTINGS_FILE = os.path.join(os.path.dirname(__file__), 'settings.txt')
-
 if __name__ == '__main__':
-    # Import settings
-    settings = {}
-    with open(SETTINGS_FILE, 'r') as file:
-        for line in file:
-            line = line.strip()
-            settings[line.split('=')[0]] = line.split('=')[1]
-    bot.run_discord_bot(token=os.getenv('BOT_TOKEN'), prefix=settings['prefix'])
+    bot.run_discord_bot(token=os.getenv('BOT_TOKEN'))
